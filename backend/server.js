07c94fa5cbd4ecedacda263012c661a91dd5b3eb
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-01-27 14:15:51 +0200

    implemented search and history endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
 import tvRoutes from "./routes/tv.routes.js";
+import searchRoutes from "./routes/search.routes.js";
 import databaseConnecting from "./utils/database.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { protect } from "./middlewares/auth.middleware.js";
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/auth", userRoutes);
 app.use("/api/movie", protect, movieRoutes);
 app.use("/api/tv", protect, tvRoutes);
+app.use("/api/search", protect, searchRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
