4c8a85f9de00beb2bc9603d872e2d7a864d3fa1b
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-01-27 14:33:23 +0200

    modified token's catching

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -5,7 +5,8 @@ import { ENV_VARS } from "../config/envVars.js";
 
 export const protect = async (req, res, next) => {
   try {
-    const token = req.cookies["token-movie"];
+    const token =
+      req.cookies["token-movie"] || req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({
         message: "Unauthorized - Token Not Found",
