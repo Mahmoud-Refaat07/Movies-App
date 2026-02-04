7fc67e7bf5a416bf38ca884ed7828c2d66f16464
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-04 13:15:36 +0200

    fixed flashing page error

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -3,7 +3,15 @@ import HomeScreen from "./HomeScreen";
 import useAuthStore from "../../store/useAuthStore";
 
 const HomePage = () => {
-  const { user } = useAuthStore();
+  const { user, loading } = useAuthStore();
+
+  if (loading) {
+    return (
+      <div className="h-screen text-white relative">
+        <div className="absolute top-0 left-0 w-full h-full bg-black flex items-center justify-center -z-10 shimmer"></div>
+      </div>
+    );
+  }
 
   return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
 };
