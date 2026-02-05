f962f8c378a7e2e3962947ef05d9bc833c16eb88
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-05 20:30:08 +0200

    refactoring

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,72 +1,78 @@
-import { use, useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Search } from "lucide-react";
 import axios from "axios";
 import { ORIGINAL_IMAGE_BASE_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
-interface searchMoviesProps {
+interface searchProps {
   id: number;
-  backdrop_path: string;
-}
-interface searchedTvShowsProps {
-  id: number;
-  backdrop_path: string;
-}
-interface searchPeopleProps {
-  id: number;
-  profile_path: string;
+  backdrop_path?: string;
+  profile_path?: string;
 }
 
 const SearchPage = () => {
   const [category, setCategory] = useState<"movie" | "tv" | "person">("movie");
   const [inputValue, setInputValue] = useState<string>("");
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [searchedMovies, setSearchedMovies] = useState<searchMoviesProps[]>([]);
-  const [searchedTvShows, setSearchedTvShows] = useState<
-    searchedTvShowsProps[]
-  >([]);
-  const [searchedPeople, setSearchedPeople] = useState<searchPeopleProps[]>([]);
+  const [searchedMovies, setSearchedMovies] = useState<searchProps[]>([]);
+  const [searchedTvShows, setSearchedTvShows] = useState<searchProps[]>([]);
+  const [searchedPeople, setSearchedPeople] = useState<searchProps[]>([]);
 
   const handleSearch = () => {
     if (!inputValue) return;
-    setSearchQuery(inputValue);
-    setInputValue("");
+
+    const fetchingData = async () => {
+      try {
+        const response = await axios.get(
+          `/api/search/${category}/${inputValue}`,
+        );
+
+        if (category === "movie") setSearchedMovies(response.data.content);
+        if (category === "tv") setSearchedTvShows(response.data.content);
+        if (category === "person") setSearchedPeople(response.data.content);
+      } catch (error: any) {
+        if (error.response && error.response.status === 404) {
+          toast.error("No results found.");
+        } else {
+          toast.error("An error occurred while fetching data.");
+        }
+      }
+    };
+    fetchingData();
   };
 
-  useEffect(() => {
-    if (!searchQuery) return;
-    const fetchingData = async () => {
-      const response = await axios.get(
-        `/api/search/${category}/${searchQuery}`,
-      );
-
-      if (category === "movie") setSearchedMovies(response.data.content);
-      if (category === "tv") setSearchedTvShows(response.data.content);
-      if (category === "person") setSearchedPeople(response.data.content);
-    };
-
-    fetchingData();
-  }, [category, searchQuery]);
   return (
     <div className="bg-black text-white min-h-screen">
       <Navbar />
       <div className="max-w-6xl mx-auto flex flex-col sm:flex-row justify-center items-center mt-8 gap-4 ">
         <button
-          className={`bg-gray-500 p-2 rounded-sm cursor-pointer hover:bg-gray-600 ${category === "movie" ? "bg-red-800" : ""}`}
-          onClick={() => setCategory("movie")}
+          className={`bg-gray-800 p-2 rounded-sm cursor-pointer hover:bg-red-600 ${category === "movie" ? "bg-red-600" : ""}`}
+          onClick={() => {
+            setCategory("movie");
+            setSearchedTvShows([]);
+            setSearchedPeople([]);
+          }}
         >
           Movies
         </button>
         <button
-          className={`bg-gray-500 p-2 rounded-sm cursor-pointer hover:bg-gray-600 ${category === "tv" ? "bg-red-800" : ""}`}
-          onClick={() => setCategory("tv")}
+          className={`bg-gray-800 p-2 rounded-sm cursor-pointer hover:bg-red-600 ${category === "tv" ? "bg-red-600" : ""}`}
+          onClick={() => {
+            setCategory("tv");
+            setSearchedMovies([]);
+            setSearchedPeople([]);
+          }}
         >
           TV Shows
         </button>
         <button
-          className={`bg-gray-500 p-2 rounded-sm cursor-pointer hover:bg-gray-600 ${category === "person" ? "bg-red-800" : ""}`}
-          onClick={() => setCategory("person")}
+          className={`bg-gray-800 p-2 rounded-sm cursor-pointer hover:bg-red-600 ${category === "person" ? "bg-red-600" : ""}`}
+          onClick={() => {
+            setCategory("person");
+            setSearchedMovies([]);
+            setSearchedTvShows([]);
+          }}
         >
           People
         </button>
@@ -74,12 +80,13 @@ const SearchPage = () => {
       <div className="max-w-2xl mx-auto mt-4 flex items-center justify-center gap-2">
         <input
           type="text"
+          placeholder={`Search for ${category === "movie" ? "Movies" : category === "tv" ? "TV Shows" : "People"}...`}
           value={inputValue}
           onChange={(e) => setInputValue(e.currentTarget.value)}
-          className="w-full p-3 bg-gray-800 rounded-sm"
+          className="w-full p-2 bg-gray-800 rounded"
         />
         <Search
-          className="bg-red-700 size-10 border border-white rounded-md cursor-pointer hover:bg-red-600"
+          className="bg-red-700 size-6 border border-white rounded-md cursor-pointer hover:bg-red-600"
           onClick={handleSearch}
         />
       </div>
