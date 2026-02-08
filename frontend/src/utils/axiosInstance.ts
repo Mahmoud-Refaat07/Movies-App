b0b079e6bf8a69a86dcdfdf1a204f0a5aa02a669
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-08 18:23:08 +0200

    fixed: cors problem with axios

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
