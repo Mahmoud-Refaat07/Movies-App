b443b3d8635b143b76ac825d8bb9775dfc5d5d17
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-08 15:56:42 +0200

    added building resoruces

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,11 @@ app.use("/api/tv", protect, tvRoutes);
 app.use("/api/search", protect, searchRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist/")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+  });
 }
 
 app.listen(PORT, () => {
