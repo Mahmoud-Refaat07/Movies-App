9dd891e2503706b2d82916eadd8ab4a57e16b2aa
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-08 18:11:33 +0200

    fixed: cors problem

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin:
+      ENV_VARS.NODE_ENV === "production"
+        ? "https://movies-app-production-3c25.up.railway.app"
+        : "http://localhost:5173",
   }),
 );
 
